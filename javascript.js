bfb9c4d7ee7f6f37db55b6175fa88975d63d8932
Fitz Hawke <will@fitzhawk.com>
2022-03-25 16:33:06 -0400

    add loop to make game best of 5

--- javascript.js
+++ javascript.js
@@ -9,50 +9,81 @@ function playerChoose() {
     choice = choice.toLowerCase();
     if (choice === "rock" || choice === "paper" || choice === "scissors") {
         return choice;
-    } else {console.log("invalid choice")}
+    } else {
+        console.log("invalid choice");
+    }
 }
 
-function playGame(player, computer) {
+function playRound(player, computer) {
+    // compare computer choice with player choice to determine winner
     switch (computer) {
         case 0: //rock
             switch (player) {
                 case "rock":
                     console.log("Draw, You both chose Rock.");
-                    break;
+                    return 0;
                 case "paper":
                     console.log("You Win! Paper beats Rock!");
-                    break;
+                    return 1;
                 case "scissors":
                     console.log("You Lose. Rock beats Scissors.");
+                    return 2;
             }
             break;
         case 1: //paper
             switch (player) {
                 case "paper":
                     console.log("Draw, You both chose Paper.");
-                    break;
+                    return 0;
                 case "scissors":
                     console.log("You Win! Scissors beats Paper!");
-                    break;
+                    return 1;
                 case "rock":
                     console.log("You Lose. Paper beats Rock.");
+                    return 2;
             }
             break;
         case 2: //scissors
             switch (player) {
                 case "scissors":
                     console.log("Draw, You both chose Scissors.");
-                    break;
+                    return 0;
                 case "rock":
                     console.log("You Win! Rock beats Scissors!");
-                    break;
+                    return 1;
                 case "paper":
                     console.log("You Lose. Scissors beats Paper.");
+                    return 2;
             }
     }
 }
 
-let compChoice = computerPlay();
-let playChoice = playerChoose();
-console.log(compChoice, playChoice);
-playGame(playChoice, compChoice);
+function playGame() {
+    console.log("Lets play Rock Paper Scissors! Best of 5 wins");
+    let pWin = 0; //Player Wins
+    let cWin = 0; //Computer Wins
+
+    //loop until either contestant has 3 wins (best of 5)
+    for (let i = 0; pWin < 3 && cWin < 3; ) {
+        const compChoice = computerPlay();
+        const playChoice = playerChoose();
+        let winner = playRound(playChoice, compChoice);
+
+        if (winner === 1) {
+            pWin++;
+        } else if (winner === 2) {
+            cWin++;
+        }
+    }
+
+    // build a statement and output the winner of the game
+    if (pWin === 3) {
+        let statement = "You win, " + pWin + " games to " + cWin;
+        console.log(statement);
+    } else if (cWin === 3) {
+        let statement = "You lose, " + cWin + " games to " + pWin;
+        console.log(statement);
+    }
+}
+
+playGame();
