2ce59ca0f74e6938ac9d33876e0063790d90698f
Fitz Hawke <will@fitzhawk.com>
2022-03-26 09:06:01 -0400

    Add basic logic for buttons and remove some unnecessary logic
    Added buttons for user input and set up logic to play the game with
    them. The game is still output in console though. Also removed
    the game tally as that is being reworked to be displayed on screen.

--- javascript.js
+++ javascript.js
@@ -10,17 +10,6 @@ function computerPlay() {
     }
 }
 
-function playerChoose() {
-    console.log("Choose Rock, Paper or Scissors");
-    let choice = prompt("What is your choice?");
-    choice = choice.toLowerCase();
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-        return choice;
-    } else {
-        console.log("invalid choice");
-    }
-}
-
 function playRound(player, computer) {
     // compare computer choice with player choice to determine winner
     switch (computer) {
@@ -65,36 +54,14 @@ function playRound(player, computer) {
     }
 }
 
-function playGame() {
-    /* console.log("Lets play Rock Paper Scissors! Best of 5 wins");
-    let pWin = 0; //Player Wins
-    let cWin = 0; //Computer Wins
+function playGame(playerChoice) {
+    console.log(playerChoice);
+    let compChoice = computerPlay();
 
-    //loop until either contestant has 3 wins (best of 5)
-    for (let i = 0; pWin < 3 && cWin < 3; ) {
-        const compChoice = computerPlay();
-        const playChoice = playerChoose();
-        let winner = playRound(playChoice, compChoice);
-
-        if (winner === 1) {
-            pWin++;
-        } else if (winner === 2) {
-            cWin++;
-        }
-    } */
-
-    const compChoice = computerPlay();
-    const playChoice = playerChoose();
-    let winner = playRound(playChoice, compChoice);
-
-    // build a statement and output the winner of the game
-    if (pWin === 3) {
-        let statement = "You win, " + pWin + " games to " + cWin;
-        console.log(statement);
-    } else if (cWin === 3) {
-        let statement = "You lose, " + cWin + " games to " + pWin;
-        console.log(statement);
-    }
+    playRound(playerChoice, compChoice);
 }
 
-playGame();
+let button = document.querySelectorAll("button");
+button.forEach((btn) =>
+    btn.addEventListener("click", (e) => playGame(e.target.className))
+);
