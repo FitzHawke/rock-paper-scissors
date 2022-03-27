c70a505d6e77fda94252e671e1dac7aa7c0cb131
Fitz Hawke <will@fitzhawk.com>
2022-03-27 02:44:57 -0400

    Set up javascript to print results
    Reorganized html and added basic css and finished javascript
    to allow the game to be played without console

--- javascript.js
+++ javascript.js
@@ -11,56 +11,63 @@ function computerPlay() {
 }
 
 function playRound(player, computer) {
+    const outcome = document.querySelector(".winner");
     // compare computer choice with player choice to determine winner
     switch (computer) {
         case "rock":
             switch (player) {
                 case "rock":
-                    console.log("Draw, You both chose Rock.");
+                    outcome.textContent = "Draw, You both chose Rock.";
                     return 0;
                 case "paper":
-                    console.log("You Win! Paper beats Rock!");
+                    outcome.textContent = "You Win! Paper beats Rock!";
                     return 1;
                 case "scissors":
-                    console.log("You Lose. Rock beats Scissors.");
+                    outcome.textContent = "You Lose. Rock beats Scissors.";
                     return 2;
             }
             break;
         case "paper":
             switch (player) {
                 case "paper":
-                    console.log("Draw, You both chose Paper.");
+                    outcome.textContent = "Draw, You both chose Paper.";
                     return 0;
                 case "scissors":
-                    console.log("You Win! Scissors beats Paper!");
+                    outcome.textContent = "You Win! Scissors beats Paper!";
                     return 1;
                 case "rock":
-                    console.log("You Lose. Paper beats Rock.");
+                    outcome.textContent = "You Lose. Paper beats Rock.";
                     return 2;
             }
             break;
         case "scissors":
             switch (player) {
                 case "scissors":
-                    console.log("Draw, You both chose Scissors.");
+                    outcome.textContent = "Draw, You both chose Scissors.";
                     return 0;
                 case "rock":
-                    console.log("You Win! Rock beats Scissors!");
+                    outcome.textContent = "You Win! Rock beats Scissors!";
                     return 1;
                 case "paper":
-                    console.log("You Lose. Scissors beats Paper.");
+                    outcome.textContent = "You Lose. Scissors beats Paper.";
                     return 2;
             }
     }
 }
 
 function playGame(playerChoice) {
-    console.log(playerChoice);
     let compChoice = computerPlay();
+    let win = playRound(playerChoice, compChoice);
 
-    playRound(playerChoice, compChoice);
+    if (win === 1) pWin++;
+    if (win === 2) cWin++;
+
+    let score = document.querySelector(".scorecard");
+    score.textContent = `${pWin} - ${cWin}`;
 }
 
+let pWin = 0;
+let cWin = 0;
 let button = document.querySelectorAll("button");
 button.forEach((btn) =>
     btn.addEventListener("click", (e) => playGame(e.target.className))
